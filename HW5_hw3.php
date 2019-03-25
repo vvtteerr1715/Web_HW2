6dc9e029c3645798db890cb0514aa887f7768dd8
vvtteerr1715 <37133516+vvtteerr1715@users.noreply.github.com>
2019-03-25 15:44:55 +0800

    Create HW5_hw3.php

---
+++ HW5_hw3.php
@@ -0,0 +1,78 @@
+<?php 
+header("Content-Type:text/html; charset=big5");
+date_default_timezone_set("Asia/Taipei");
+$year= $_GET["year"];
+$month= $_GET["month"];
+
+    //可取得該月第一天的時間戳記
+
+    $days =strtotime(date("$year-$month-01"));
+
+    //可取得該月1號是該週的星期幾
+
+    $startweek = date("w",$days);
+	
+	//可取得該月有幾天
+	$date =date("t",$days);	
+	
+ echo "<center>";
+	echo "<caption><font size='5'> $year 年 $month 月份月曆 </font></caption>";
+    echo '<table border="2" width="500" align="center">';
+	
+    echo '<tr>';
+
+    echo '<th bgcolor="pink">星期日</th>';
+
+    echo '<th bgcolor="pink">星期一</th>';
+
+    echo '<th bgcolor="pink">星期二</th>';
+
+    echo '<th bgcolor="pink">星期三</th>';
+
+    echo '<th bgcolor="pink">星期四</th>';
+
+    echo '<th bgcolor="pink">星期五</th>';
+
+    echo '<th bgcolor="pink">星期六</th>';    
+
+    echo '</tr>';
+
+    echo '<tr>';
+
+    for($i=0;$i<$startweek;$i++){
+
+        echo "<td>&nbsp;</td>";
+
+    }
+
+    for ($j=1;$j<=$date;$j++){
+
+        $i++;
+
+      
+        echo "<td bgcolor='yellow'>{$j}</td>";
+
+        
+
+        if($i%7 == 0){
+
+            echo '</tr><tr>';
+
+        }
+
+    }
+
+    while($i%7!==0){
+
+        echo '<td>&nbsp;</td>';
+
+        $i++;
+
+    }
+
+    echo '</tr>';
+
+    echo '</table>';
+	echo "</center>";
+?>
+
